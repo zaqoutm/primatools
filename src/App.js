088981651cbb6160ccd6b22b6aede7de497d8c4a
Mo Zaqout <mo.zaqout@gmail.com>
2022-06-17 10:43:23 +0300

    updated welcome text

--- src/App.js
+++ src/App.js
@@ -9,27 +9,15 @@ function App() {
     const appsWeCreate = ['Web', 'Mobile']
 
     useEffect(() => {
-        gsap.timeline()
+        // gsap.timeline()
         // .fromTo('.app', {opacity: 0}, {opacity: 0.2, duration: 0.9})
         // .to('.app', {opacity: 1, y: 0, duration: .2})
 
         // var vid = document.getElementById("space-video");
         // vid.playbackRate = 0.4;
+        gsap.timeline().set(['.item1', '.item2'], {opacity: 0, y: 58})
 
-        gsap.timeline()
-            .set('.cursor1', {opacity: .2})
-            .fromTo('.cursor1', {opacity: 0}, {opacity: 1, repeat: 3})
-            .to('.cursor1', {x: '94%', duration: 2.2})
-            .to('.cursor1', {opacity: 0, duration: 0})
-
-        let crsrTL = gsap.timeline()
-            .set('.cursor2', {opacity: 0})
-            .to('.cursor2', {delay: 4, x: '52%', duration: 1.4, opacity: 1})
-            .to('.cursor2', {opacity: 0, scale: 2, x: '100%', duration: .4})
-
-        gsap.timeline().set(['.item1', '.item2'], {opacity: 0, y: 38})
-
-        let appsTL = gsap.timeline({repeat: -1, repeatDelay: 0})
+        let appsTL = gsap.timeline({delay: 4, repeat: -1, repeatDelay: 0})
         let item1TL = gsap.timeline()
             .to('.item1', {y: 0, opacity: 1, duration: .2})
             .to('.item1', {delay: 1, duration: .2, opacity: 0})
@@ -38,7 +26,6 @@ function App() {
             .to('.item2', {delay: 1, duration: .2, opacity: 0})
         appsTL.add(item1TL)
         appsTL.add(item2TL)
-        crsrTL.add(appsTL)
 
     }, [])
 
@@ -60,20 +47,30 @@ function App() {
 
                 <div className={'welcome'}>
                     <div className="centered-content relative">
-                        <Tween from={{opacity: 0}} stagger={.8}
-                               duration={3} delay={2}>
-                            <SplitChars wrapper={<span className={'hello-text'}/>}>
-                                Hello,&nbsp;We&nbsp;are&nbsp;a&nbsp;software&nbsp;company,
-                                &#10;and&nbsp;we&nbsp;create&nbsp;apps&nbsp;
-                            </SplitChars>
-                        </Tween>
 
-                        <p className={'cursor1'}>
-                            <span>|</span>
-                        </p>
-                        <p className={'cursor2'}>
-                            <span>|</span>
-                        </p>
+                        <div className={'hello-text-wrapper'}>
+                            <div>
+                                <Tween from={{opacity: 0}} stagger={.8} duration={.2} delay={1}>
+                                    <SplitChars wrapper={<span className={'hello-text'}/>}>
+                                        Hello,
+                                    </SplitChars>
+                                </Tween>
+                            </div>
+                            <div>
+                                <Tween from={{opacity: 0}} stagger={.8} duration={.4} delay={2}>
+                                    <SplitChars wrapper={<span className={'hello-text'} style={{fontSize: 28}}/>}>
+                                        We&nbsp;are&nbsp;a&nbsp;software&nbsp;company
+                                    </SplitChars>
+                                </Tween>
+                            </div>
+                            <div>
+                                <Tween from={{opacity: 0}} stagger={.8} duration={.4} delay={3}>
+                                    <SplitChars wrapper={<span className={'hello-text'} style={{fontSize: 28}}/>}>
+                                        &#13;and&nbsp;we&nbsp;create&nbsp;apps&nbsp;
+                                    </SplitChars>
+                                </Tween>
+                            </div>
+                        </div>
 
                         <div className={'welcome-apps-list'}>
                             {appsWeCreate.map((a, i) =>
@@ -81,7 +78,6 @@ function App() {
                                     <span>for</span><p>{a}</p>
                                 </div>)}
                         </div>
-
                     </div>
 
                 </div>
@@ -133,6 +129,7 @@ function App() {
                             <h4>Who We Are</h4>
                             <a href="#">About PrimaTools</a>
                             <a href="#">Careers</a>
+                            <a href="#">Blogs</a>
                         </div>
                         <div className={'footer-links'}>
                             <h4>Contacts</h4>
