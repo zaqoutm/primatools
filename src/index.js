15b3ba661afca142c97d59f1bb25ba39dbc7cdd0
Mo Zaqout <mo.zaqout@gmail.com>
2022-11-12 13:56:37 +0300

    phone css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import './styles/hello.css';
 import './styles/services.css';
+import './styles/phone.css';
 import App from './App';
 
 ReactDOM.render(
